e407d91b9efd963d48efd3d6014e6e475bbd7a09
kadzahk <juan427lol@gmail.com>
2022-06-21 22:08:56 -0500

    library done

---
+++ sort.h
@@ -0,0 +1,35 @@
+
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+listint_t *swap(listint_t *list, listint_t *node, int node_dir);
+void selection_sort(int *array, size_t size);
+void simple_swap(int *l1, int *l2);
+void quick_sort(int *array, size_t size);
+void partition_sort(int array_lim[], int start, int end, size_t size);
+int lim(int array_lim[], int start, int end, size_t size);
+
+
+
+#endif
